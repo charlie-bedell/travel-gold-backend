c4d28f27c25b88ae176f94dc55e2f8c3490d8ad5
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2022-07-11 10:34:47 -0500

    done with step 2

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+const SALT_ROUNDS = 6
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -7,6 +9,20 @@ const userSchema = new mongoose.Schema({
   profile: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'}
 }, {
   timestamps: true,
+})
+
+userSchema.pre('save', function(next) {
+  const user = this
+  if (!user.isModified('password')) return next()
+  bcrypt.hash(user.password, SALT_ROUNDS)
+  .then(hash => {
+    user.password = hash
+    next()
+  })
+  .catch(err => {
+    console.log(err)
+    next(err)
+  })
 })
 
 userSchema.set('toJSON', {
