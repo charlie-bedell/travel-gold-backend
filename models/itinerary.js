7bd1538e046acb814a4818a2df09c11a9cd99541
Charles Bedell <charlesbedell5@gmail.com>
2024-01-23 18:25:35 -0500

    add isPublic field

--- models/itinerary.js
+++ models/itinerary.js
@@ -5,7 +5,11 @@ const Schema = mongoose.Schema;
 const itinerarySchema = new Schema ({
   name: String,
   profile_id: {type: mongoose.Schema.Types.ObjectId, ref: 'Profile'},
-  poi_ids: [{type: mongoose.Schema.Types.ObjectId, ref: 'POI'}]
+  poi_ids: [{type: mongoose.Schema.Types.ObjectId, ref: 'POI'}],
+  isPublic: Boolean
+});
+
+itinerarySchema.post('findOneAndDelete', async (itinerary, next) => {
 });
 
 const Itinerary = mongoose.model('Itinerary', itinerarySchema);
