57ad0d32e51bedc5094ff38163a1c661a4caafbb
Shyan W. <shyanw321@gmail.com>
2024-01-23 16:15:56 -0500

    Added Google Nearby Search

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import 'dotenv/config.js';
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import axios  from 'axios';
 
 // connect to MongoDB with mongoose
 import './config/database.js';
@@ -14,6 +15,7 @@ import { router as authRouter } from './routes/auth.js';
 // create the express app
 const app = express();
 
+
 // basic middleware
 app.use(cors());
 app.use(logger('dev'));
@@ -23,14 +25,29 @@ app.use(express.json());
 app.use('/api/profiles', profilesRouter);
 app.use('/api/auth', authRouter);
 
-// handle 404 errors
-app.use(function (req, res, next) {
-  res.status(404).json({ err: 'Not found' });
-});
+
 
 // handle all other errors
 app.use(function (err, req, res, next) {
   res.status(err.status || 500).json({ err: err.message });
+});
+
+app.get('/api/nearbySearch/', async (req, res) => {
+  const { lat, lng, query } = req.query;
+  try {
+    console.log(req.query)
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=2000&keyword=${encodeURIComponent(query)}&key=${process.env.GOOGLE_MAPS_API_KEY}`);
+    res.json(response.data.results);
+    console.log(response.data.results)
+    console.log("done")
+  } catch (error) {
+    res.status(500).send('Error fetching nearby places');
+  }
+});
+
+// handle 404 errors
+app.use(function (req, res, next) {
+  res.status(404).json({ err: 'Not foundD' });
 });
 
 export { app }
