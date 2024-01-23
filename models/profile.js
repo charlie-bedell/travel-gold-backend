f109d85bf2f741a9740888d86bffd4c8d312180f
Charles Bedell <charlesbedell5@gmail.com>
2024-01-22 19:27:44 -0500

    scaffold for itinerary and poi models

--- models/profile.js
+++ models/profile.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const profileSchema = new Schema({
-  name: String,
+  first_name: String,
+  last_name: String,
+  itinerary_ids: [{type: mongoose.Schema.Types.ObjectId, ref: 'Itinerary'}]
 },{
   timestamps: true,
 });
