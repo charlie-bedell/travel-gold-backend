4183ca896781352707e0b435d88516881c3de33b
Charles Bedell <charlesbedell5@gmail.com>
2024-01-24 17:00:19 -0500

    update fields to pull from google api and label them in poiSchema

--- controllers/poi.js
+++ controllers/poi.js
@@ -6,7 +6,27 @@ import axios from 'axios';
 function getPlace(req,res) {
   const place_id = req.params.place_id;
   const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
-  const placeFields = ['id', 'displayName'];
+  
+  const placeFields = ['id',
+                       'displayName',
+                       'rating',
+                       'editorialSummary',
+                       'regularOpeningHours.weekdayDescriptions',
+                       'websiteUri',
+                       'business_status',
+                       'formattedAddress',
+                       'internationalPhoneNumber',
+                       'nationalPhoneNumber',
+                       'regularOpeningHours',
+                       'serves_beer',
+                       'serves_breakfast',
+                       'serves_lunch',
+                       'serves_dinner',
+                       'serves_wine',
+                       'price_level',
+                       'reservable',
+                       'types'];
+
   console.log('place_id: ', place_id);
   console.log('GOOGLE_MAPS_API_KEY: ', GOOGLE_MAPS_API_KEY);
 
