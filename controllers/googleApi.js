9c908a3eae4bd5bb17def525abb593b2b53d9921
Charles Bedell <charlesbedell5@gmail.com>
2024-01-26 10:00:39 -0500

    change neraby search back to await from then

--- controllers/googleApi.js
+++ controllers/googleApi.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-function getNearbyPlaces(req,res) {
+async function getNearbyPlaces(req,res) {
   const { lat, lng, query } = req.query;
-  axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=2000&keyword=${encodeURIComponent(query)}&key=${process.env.GOOGLE_MAPS_API_KEY}`).then((response) => {
+  try {
+    console.log('query received: ', req.query);
+    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=2000&keyword=${encodeURIComponent(query)}&key=${process.env.GOOGLE_MAPS_API_KEY}`);
     res.status(200).json(response.data.results);
-  })
-  .catch((error) => {
-    res.status(500).send('error fetching nearby places');
-  });
+  } catch (err) {
+    res.status(500).json({message: "Error fetching nearby places", error: `${err}`});
+  }
 };
 
 export { getNearbyPlaces }
